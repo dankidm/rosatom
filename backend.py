cc45852e83c4e4b2609399e8e5b45adf1e70f996
roffio <skuzmicheff@gmail.com>
2024-12-01 10:45:31 +0300

    backend fix truee

---
+++ backend.py
@@ -0,0 +1,156 @@
+print("Booting up the backend...\nPlease, don't forget to add the weights and latest checkpoint to the project folder!\n")
+
+import sys
+import os.path
+from os.path import exists
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../cv')))
+
+
+from flask import Flask, request, jsonify
+import base64
+import hashlib
+
+from cv.car_classification import get_car_type
+from cv.img_to_segments import img_to_segments
+from cv.poligons_from_seg import image_to_polygon_vertices
+from cv.segment_interface import interface_start
+
+
+app = Flask(__name__)
+
+processing_status = {} # Uploaded Processing Ready or Error
+processed_segments = {}
+
+if not os.path.exists("img"):
+    os.mkdir("img")
+if not os.path.exists("seg_img"):
+    os.mkdir("seg_img")
+
+def get_segments_from_image(output_file, image_hash, path):
+    # return image_to_polygon_vertices(output_file)
+    return img_to_segments(output_file, image_hash, path)
+
+def process_image(image_hash, image_data):
+    processing_status[image_hash] = "Processing"
+    try:
+        initial_file = os.path.join("img", f"{image_hash}.png")
+        initial_file_split = os.path.join("img", f"{image_hash}_split.png")
+        seg_dir = os.path.join("seg_img", image_hash)
+
+        # Save initial image
+        with open(initial_file, "wb") as file:
+            file.write(image_data)
+
+        # Ensure the segmentation directory exists
+        os.makedirs(seg_dir, exist_ok=True)
+
+        processed_segments[image_hash] = {}
+        processed_segments[image_hash]["Type"] = get_car_type(initial_file)
+
+        # Start processing
+        interface_start(initial_file, initial_file_split)
+
+        # Process and store segments
+        img_to_segments(initial_file_split, image_hash, seg_dir)
+
+
+        for segment_file_name in os.listdir(seg_dir):
+            segment_path = os.path.join(seg_dir, segment_file_name)
+            processed_segments[image_hash][segment_file_name] = image_to_polygon_vertices(segment_path)
+
+
+        # Cleanup temporary files and directory
+        for segment_file_name in os.listdir(seg_dir):
+            os.remove(os.path.join(seg_dir, segment_file_name))
+        os.rmdir(seg_dir)
+
+        os.remove(initial_file)
+        os.remove(initial_file_split)
+
+        processing_status[image_hash] = "Ready"
+    except Exception as e:
+        processing_status[image_hash] = "Error"
+        print(f"Exception occurred when processing image {image_hash}: {e}")
+
+
+# POST "/" - загрузка изображения и начало обработки
+@app.route('/', methods=['POST'])
+def upload_image():
+    try:
+        data = request.json
+        base64_image = data.get('image', '')
+
+        if not base64_image:
+            return jsonify({'error': 'No image provided'}), 400
+
+        image_data = base64.b64decode(base64_image)
+        image_hash = hashlib.sha256(image_data).hexdigest()
+
+        if image_hash in processed_segments:
+            return jsonify({'message': f"Image already calculated. just use it",'image_hash':image_hash}), 200
+
+        processing_status[image_hash] = "Uploaded"
+        process_image(image_hash, image_data)
+
+        return jsonify({'message': 'Image uploaded successfully', 'image_hash':image_hash}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+# @app.route('/', methods=['POST'])
+# def upload_image():
+#     try:
+#         data = request.json
+#         image_path = data.get('image_path', '')
+#
+#         if not image_path or not os.path.isfile(image_path):
+#             return jsonify({'error': 'Invalid or missing image path'}), 400
+#
+#         with open(image_path, 'rb') as img_file:
+#             image_data = img_file.read()
+#
+#         image_hash = hashlib.sha256(image_data).hexdigest()
+#
+#         if image_hash in processed_segments:
+#             return jsonify({'message': f"Image already calculated. Just use it", 'image_hash': image_hash}), 200
+#
+#         processing_status[image_hash] = "Uploaded"
+#         process_image(image_hash, image_data)
+#
+#         return jsonify({'message': 'Image uploaded successfully', 'image_hash': image_hash}), 200
+#
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
+
+
+# GET "/" - возвращает все созданные идентификаторы
+@app.route('/', methods=['GET'])
+def get_image():
+    output = list()
+    for i in processing_status:
+        output.append(i)
+    return jsonify(output), 200
+
+
+# GET "/<id>/status" - статус обработки
+@app.route("/<image_id>/status", methods=["GET"])
+def get_status(image_id):
+    if image_id not in processing_status:
+        return jsonify({"error": "Image ID not found"}), 404
+
+    return jsonify({"status": processing_status[image_id]})
+
+
+# GET "/<id>/segments" - сегменты изображения
+@app.route("/<image_id>/segments", methods=["GET"])
+def get_segments(image_id):
+    if image_id not in processed_segments:
+        if image_id in processing_status and processing_status[image_id] == "processing":
+            return jsonify({"error": "Processing not completed yet"}), 400
+        return jsonify({"error": "Image ID not found"}), 404
+    return jsonify({"segments": processed_segments[image_id]})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ cv/segment_interface.py
@@ -0,0 +1,100 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+from segmentation_models_pytorch import MAnet
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+ENCODER = "resnet34"  # Должен соответствовать энкодеру, для которого были сохранены веса
+CLASSES = 22  # Количество классов сегментации (включая фон)
+ACTIVATION = "sigmoid"  # Активация на выходе
+
+# Создание модели
+model = MAnet(
+    # encoder_name=ENCODER,
+    # encoder_weights=None,  # Предобученные веса не загружаем здесь
+    classes=CLASSES,
+    # activation=ACTIVATION
+)
+
+# Путь к файлу с весами
+weights_path = "weights/model_weights.pth"
+
+# Загрузка весов
+try:
+    checkpoint = torch.load(weights_path, map_location=torch.device("cpu"))
+
+    # Если файл содержит дополнительные ключи, такие как "model_state_dict"
+    if "model_state_dict" in checkpoint:
+        state_dict = checkpoint["model_state_dict"]  # Извлекаем только параметры модели
+    else:
+        state_dict = checkpoint  # Если файл содержит только state_dict
+
+    # Загрузка весов в модель
+    model.load_state_dict(state_dict)
+    print("Weights loaded successfully.")
+except FileNotFoundError:
+    print(f"Error: Weights file not found at {weights_path}.")
+    exit()
+except Exception as e:
+    print(f"Error loading weights: {e}")
+    exit()
+
+# Переключение модели в режим оценки
+model.eval()
+
+
+def load_image(image_path, transform=None):
+    with Image.open(image_path).convert("RGB") as image:
+        image = Image.open(image_path).convert("RGB")
+    if transform:
+        image = transform(image)
+    return image
+
+
+transform = transforms.Compose([
+    transforms.Resize((320, 320)),  # Изменение размера
+    transforms.ToTensor(),  # В тензор
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Нормализация
+])
+
+
+
+def save_multiclass_segmentation(output, output_path, num_classes=22):
+    """
+    Сохраняет многоклассовую сегментацию с уникальными цветами для каждого класса.
+
+    :param output: Сегментированная маска (numpy array), значения в диапазоне [0, num_classes).
+    :param output_path: Путь для сохранения результата.
+    :param num_classes: Общее количество классов.
+    """
+    # Определяем цвета для классов
+    np.random.seed(42)  # Для детерминированности
+    class_colors = np.random.randint(0, 255, size=(num_classes, 3), dtype=np.uint8)
+
+    # Создаём цветную маску
+    height, width = output.shape
+    mask = np.zeros((height, width, 3), dtype=np.uint8)
+
+    for cls in range(num_classes):
+        mask[output == cls] = class_colors[cls]
+
+    # Сохранение результата
+    segmented_image = Image.fromarray(mask)
+    segmented_image.save(output_path)
+    print(f"Segmented image saved to {output_path}")
+
+
+def interface_start(input_image_path, output_path):
+    image_path = input_image_path
+
+    input_image = load_image(image_path, transform=transform)
+    input_image = input_image.unsqueeze(0).to(device)
+    with torch.no_grad():
+        output = model(input_image)
+        output = torch.argmax(output, dim=1).squeeze().cpu().numpy()  # Получаем классы для каждого пикселя
+
+    save_multiclass_segmentation(output, output_path, num_classes=22)
